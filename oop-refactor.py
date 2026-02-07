b87767e8550281d68e33d4e999afc967ec213bd9
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-02-07 08:12:15 -0300

    refactor: make OOP structure file to migrate main.py

---
+++ oop-refactor.py
@@ -0,0 +1,8 @@
+import os, sys, subprocess, tkinter as tk
+from tkinter import Label, Entry, Tk, BOTH, Button, Frame, X, messagebox, filedialog, ttk
+
+# Transitional file from procedural to OOP, bound to replace main.py
+
+class EasyDLPApp:
+    def __init__(self):
+        pass
