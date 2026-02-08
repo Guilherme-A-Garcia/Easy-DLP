28ff817b3ef936455cbcf5d709af3ee914849a30
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-02-08 05:25:53 -0300

    refactor: improve transitional file structure to fit later dependency injection

--- oop-refactor.py
+++ oop-refactor.py
@@ -3,6 +3,45 @@ from tkinter import Label, Entry, Tk, BOTH, Button, Frame, X, messagebox, filedi
 
 # Transitional file from procedural to OOP, bound to replace main.py
 
+def main():
+    app = EasyDLPApp()
+
 class EasyDLPApp:
     def __init__(self):
+        self.root = tk.Tk()
+        self.root.withdraw()
+        
+        self.root.mainloop()
+    
+    def show_cache_window(self):
         pass
+    
+    def show_cookie_window(self):
+        pass
+    
+    def show_main_window(self):
+        pass
+
+    def close_current(self):
+        pass
+    
+class CacheWindow(tk.Toplevel):
+    def __init__(self, app):
+        super().__init__(app.root)
+        self.app = app
+        pass
+    
+class CookieWindow(tk.Toplevel):
+    def __init__(self, app):
+        super().__init__(app.root)
+        self.app = app
+        pass
+    
+class MainWindow(tk.Toplevel):
+    def __init__(self, app):
+        super().__init__(app.root)
+        self.app = app
+        pass
+    
+if __name__ == "__main__":
+    main()
