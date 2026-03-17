3585ca5eb8285ee3772a6c19d3b8b9cbebf73b4c
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-03-17 14:03:52 -0300

    feat: import urllib.request

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from CTkMessagebox import CTkMessagebox
 from PIL import Image, ImageTk
-import customtkinter as ctk
-import requests
 from bs4 import BeautifulSoup
+import customtkinter as ctk
+import urllib.request
 import subprocess
 import threading
+import requests
 import sys
 import os
 
