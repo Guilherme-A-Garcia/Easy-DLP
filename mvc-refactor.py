434a549d02bf54ae82bc585f7e9677afc4254653
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-03-26 14:25:03 -0300

    chore: import the project's libraries

--- mvc-refactor.py
+++ mvc-refactor.py
@@ -1,2 +1,12 @@
+from CTkMessagebox import CTkMessagebox
+from PIL import Image, ImageTk
+from bs4 import BeautifulSoup
+import customtkinter as ctk
+import urllib.request
+import subprocess
+import threading
+import requests
+import sys
+import os
 
 
